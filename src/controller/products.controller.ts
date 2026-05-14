dcca249746ad9798965c67ab7c2c5d93544da2fa
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-05-15 00:36:40 +0600

    learned how to delete with DELETE method and test with postman ... perfectly worked

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -17,24 +17,7 @@ export const productsController = async (
   // console.log(id, typeof(id));
 
   if (url === "/products" && method === "GET") {
-    // get all products by get method
-    // const products = [
-    //     {
-    //         id : 1,
-    //         name : "Product 1"
-    //     },
-    //     {
-    //         id : 2,
-    //         name : "Product 2"
-    //     },
-    //     {
-    //         id : 3,
-    //         name : "Product 3"
-    //     }
-    // ];
-
     const products = readProduct();
-
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({ message: "Data retrived succesfully", data: products }),
@@ -44,13 +27,19 @@ export const productsController = async (
     const products = readProduct();
     const product = products.find((p: Iproduct) => p.id === id);
     // console.log(product);
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(
-      JSON.stringify({
-        message: "This product retrived successfully",
-        product: product,
-      }),
-    );
+    
+    if (!product) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+    } else {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "This product retrived successfully",
+          product: product,
+        }),
+      );
+    }
   } else if (method === "POST" && url === "/products") {
     // create product by post method
     const body = await parseBody(req);
@@ -74,21 +63,43 @@ export const productsController = async (
     const products = readProduct();
     const index = products.findIndex((p: Iproduct) => p.id === id);
     // console.log(index);
-    if(index < 0){
-        res.writeHead(404, {"content-type" : "application/json"});
-        res.end(JSON.stringify({message : "Product not found"}));
-    }else{
-        // console.log(products[index]);
-        products[index] = {
-            id : products[index].id , 
-            ...body
-        };
-        insertProduct(products);
-        res.writeHead(200 , {"content-type" : "application/json"});
-        res.end(JSON.stringify({
-            message : "Product updated successfully",
-            data : products[index]
-        }))
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+    } else {
+      // console.log(products[index]);
+      products[index] = {
+        id: products[index].id,
+        ...body,
+      };
+      insertProduct(products);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product updated successfully",
+          data: products[index],
+        }),
+      );
+    }
+  } else if (method === "DELETE" && id !== null) {
+    const products = readProduct();
+    const index = products.findIndex((P: Iproduct) => P.id === id);
+    // console.log(index);
+
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+    } else {
+      products.splice(index, 1);
+      // console.log(products);
+      insertProduct(products);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product deleted successfully",
+          data: products,
+        }),
+      );
     }
   }
 };
