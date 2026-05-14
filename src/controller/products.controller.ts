bc3d96fa8bd02be5aeb20e38361136cc32671e92
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-05-15 00:18:08 +0600

    learned how to change or update a specific product and test with postman

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -3,52 +3,92 @@ import { insertProduct, readProduct } from "../service/product.service";
 import type { Iproduct } from "../types/product.type";
 import { parseBody } from "../utility/parseBody";
 
-export const productsController = async(req : IncomingMessage , res : ServerResponse) =>{
-    const url = req.url;
-    const method = req.method;
-    const urlParts = url?.split('/');
-    // console.log(urlParts);
-    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null;
+export const productsController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  const url = req.url;
+  const method = req.method;
+  const urlParts = url?.split("/");
+  // console.log(urlParts);
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
-    // console.log(id, typeof(id));
+  // console.log(id, typeof(id));
 
-    if(url === '/products' && method === "GET"){
-        // const products = [
-        //     {
-        //         id : 1,
-        //         name : "Product 1"
-        //     },
-        //     {
-        //         id : 2,
-        //         name : "Product 2"
-        //     },
-        //     {
-        //         id : 3,
-        //         name : "Product 3"
-        //     }
-        // ];
+  if (url === "/products" && method === "GET") {
+    // get all products by get method
+    // const products = [
+    //     {
+    //         id : 1,
+    //         name : "Product 1"
+    //     },
+    //     {
+    //         id : 2,
+    //         name : "Product 2"
+    //     },
+    //     {
+    //         id : 3,
+    //         name : "Product 3"
+    //     }
+    // ];
 
-        const products = readProduct();
+    const products = readProduct();
 
-        res.writeHead(200 , {"content-type" : "application/json"});
-        res.end(JSON.stringify({message : "Data retrived succesfully" , data : products}));
-    }else if(method === 'GET' && id !== null){
-        const products = readProduct();
-        const product = products.find((p : Iproduct) => p.id === id);
-        // console.log(product);
-        res.writeHead(200 , {"content-type" : "application/json"});
-        res.end(JSON.stringify({message : "This product retrived successfully" , product : product}));
-    }else if (method === 'POST' && url === '/products'){
-        const body = await parseBody(req);
-        const products = readProduct();
-        // console.log(body);
-        const newProduct ={
-            id : Date.now(),
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "Data retrived succesfully", data: products }),
+    );
+  } else if (method === "GET" && id !== null) {
+    // get single product by get method
+    const products = readProduct();
+    const product = products.find((p: Iproduct) => p.id === id);
+    // console.log(product);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "This product retrived successfully",
+        product: product,
+      }),
+    );
+  } else if (method === "POST" && url === "/products") {
+    // create product by post method
+    const body = await parseBody(req);
+    const products = readProduct();
+    // console.log(body);
+    const newProduct = {
+      id: Date.now(),
+      ...body,
+    };
+    products.push(newProduct);
+    insertProduct(products);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "product created successfully",
+        data: products,
+      }),
+    );
+  } else if (method === "PUT" && id !== null) {
+    const body = await parseBody(req);
+    const products = readProduct();
+    const index = products.findIndex((p: Iproduct) => p.id === id);
+    // console.log(index);
+    if(index < 0){
+        res.writeHead(404, {"content-type" : "application/json"});
+        res.end(JSON.stringify({message : "Product not found"}));
+    }else{
+        // console.log(products[index]);
+        products[index] = {
+            id : products[index].id , 
             ...body
-        }
-        products.push(newProduct);
+        };
         insertProduct(products);
         res.writeHead(200 , {"content-type" : "application/json"});
-        res.end(JSON.stringify({message : "product created successfully" , data : products}));
+        res.end(JSON.stringify({
+            message : "Product updated successfully",
+            data : products[index]
+        }))
     }
-}
+  }
+};
